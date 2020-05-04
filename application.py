f4f275e607d6337d8f91ab1e478bb20074c63124
Sammy-Barasa <kesammybarasa@gmail.com>
2020-05-04 18:24:44 +0300

    added sqlachemy trackmodifications-false

--- application.py
+++ application.py
@@ -26,6 +26,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
 
 # bcrypt = Bcrypt(app)
